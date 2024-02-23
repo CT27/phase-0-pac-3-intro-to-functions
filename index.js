98b8e87aff5545104dee5d2123963268dfb1148a
Candice Thomason <candice.thomason@gmail.com>
2024-02-24 10:39:46 +1100

    adds

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 // Follow along with the examples here
-
 function sayHello() {
     console.log("Hello!");
   }
+
+  sayHello();
+
+  function sayHelloToGuadalupe() {
+    console.log("Hello, Guadalupe!");
+  }
+
+  sayHelloToGuadalupe();
